0a036ceb2d287c34913053e3249e2602a0df1d82
Drak <enrique.almeida@desoft.cav.cu>
2022-02-26 12:03:17 +0100

    Incluido en el código la duración de las rachas de más de un día.

--- Rainy Streak_class.py
+++ Rainy Streak_class.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+from numba import jit
 
 
 # import pickle
@@ -17,8 +18,7 @@ class Rainy_Streak:
         self.df_final = pd.DataFrame(columns=['rainy_days_count', 'rainy_streak_count', 'rainy_streak_med_long'])
 
         for month in self.separated_months:
-            rainy_days_count, rainy_streak_count, rainy_streak_med_long, streak_counter_if_length_more_than_one, irregularity_index = \
-                self.get_rainy_streak_data(month)
+            rainy_days_count, rainy_streak_count, rainy_streak_med_long, streak_counter_if_length_more_than_one, irregularity_index, streak_avg_lenght_if_length_more_than_one = self.get_rainy_streak_data(month)
 
             rain_probability = rainy_days_count / len(month)
 
@@ -32,7 +32,8 @@ class Rainy_Streak:
                                                   'rain_probability_if_before': rain_probability_if_before,
                                                   'coef_rainy_besson': coef_rainy_besson,
                                                   'streak_counter_if_length_more_than_one': streak_counter_if_length_more_than_one,
-                                                  'irregularity_index': irregularity_index
+                                                  'irregularity_index': irregularity_index,
+                                                  'streak_avg_lenght_if_length_more_than_one': streak_avg_lenght_if_length_more_than_one
                                                   },
                                                  ignore_index=True)
 
@@ -44,7 +45,8 @@ class Rainy_Streak:
         self.coef_rainy_besson_mean = self.df_final['coef_rainy_besson'].mean()
         self.streak_counter_if_length_more_than_one_mean = self.df_final['streak_counter_if_length_more_than_one'].mean()
         self.irregularity_index_mean = self.df_final['irregularity_index'].mean()
-
+        self.streak_avg_lenght_if_length_more_than_one_mean = self.df_final.streak_avg_lenght_if_length_more_than_one[self.df_final.streak_avg_lenght_if_length_more_than_one > 0].mean()
+        
         # self.info_on_a_row = pd.Series(precip_data['Registro'])
         # self.file_name = file_name
         # self.max_registered_value = self.info_on_a_row.max()
@@ -92,6 +94,7 @@ class Rainy_Streak:
                 precip_sum = 0
         return list_to_return
 
+    @jit(cache = True)
     def get_rainy_streak_data(self, data):
         my_stack = list(data)
         rainy_days_count = 0
@@ -100,6 +103,7 @@ class Rainy_Streak:
         accumulated = 0
         streak_lenght = 0
         streak_counter_if_length_more_than_one = 0
+        streak_lenght_if_length_more_than_one = 0
         sum_ln_Pi = 0
         while my_stack:
             s = my_stack.pop(0)
@@ -114,6 +118,7 @@ class Rainy_Streak:
                     rainy_days_count += streak_lenght
                     if streak_lenght > 1:
                         streak_counter_if_length_more_than_one += 1
+                        streak_lenght_if_length_more_than_one += streak_lenght
                     # print(full_result)
                 accumulated = 0
                 streak_lenght = 0
@@ -121,8 +126,10 @@ class Rainy_Streak:
         rainy_streak_count = len(full_result)
         rainy_streak_med_long = 0 if rainy_streak_count == 0 else rainy_days_count / rainy_streak_count
         irregularity_index = (1/(len(data)-1))*sum_ln_Pi
-        return rainy_days_count, rainy_streak_count, rainy_streak_med_long, \
-               streak_counter_if_length_more_than_one, irregularity_index
+        streak_avg_lenght_if_length_more_than_one = 0 if not streak_counter_if_length_more_than_one else streak_lenght_if_length_more_than_one/streak_counter_if_length_more_than_one
+        
+        return rainy_days_count, rainy_streak_count, rainy_streak_med_long, streak_counter_if_length_more_than_one, irregularity_index, streak_avg_lenght_if_length_more_than_one
+               
 
 
 excel_path = 'Full_data.xlsx'
@@ -148,6 +155,7 @@ with pd.ExcelWriter('output.xlsx') as writer:
             rainy_streak.rainy_days_mean,
             rainy_streak.rainy_streak_mean,
             rainy_streak.streak_counter_if_length_more_than_one_mean,
+            rainy_streak.streak_avg_lenght_if_length_more_than_one_mean,
             rainy_streak.rainy_streak_med_long_mean,
             rainy_streak.rain_probability_mean,
             rainy_streak.rain_probability_if_before_mean,
@@ -157,6 +165,7 @@ with pd.ExcelWriter('output.xlsx') as writer:
             columns=['Promedio de días lluviosos',
                      'Cantidad promedio de rachas',
                      'Cantidad promedio de rachas de más de un día',
+                     'Duración media de las rachas de más de un día',
                      'Duración media de las rachas (Todas)',
                      'Probabilidad media de días lluviosos',
                      'Probabilidad media de días lluviosos si llovió el anterior',
